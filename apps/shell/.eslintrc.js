e2a03e9b7b5eca07b8f5256c291ab78f4095516b
Sujay Naik <sujaynaik142@gmail.com>
2026-07-25 00:35:24 +0530

    feat: add typescript path aliases and architecture boundaries2

---
+++ apps/shell/.eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  plugins: [
+    "boundaries"
+  ],
+
+  settings: {
+    "boundaries/elements": [
+      {
+        type: "app",
+        pattern: "src/app/*"
+      },
+      {
+        type: "pages",
+        pattern: "src/pages/*"
+      },
+      {
+        type: "features",
+        pattern: "src/features/*"
+      },
+      {
+        type: "shared",
+        pattern: "src/shared/*"
+      }
+    ]
+  },
+
+  rules: {
+    "boundaries/element-types": [
+      "error",
+      {
+        default: "disallow",
+
+        rules: [
+          {
+            from: "app",
+            allow: [
+              "pages",
+              "features",
+              "shared"
+            ]
+          },
+          {
+            from: "pages",
+            allow: [
+              "features",
+              "shared"
+            ]
+          },
+          {
+            from: "features",
+            allow: [
+              "shared"
+            ]
+          },
+          {
+            from: "shared",
+            allow: [
+              "shared"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
