ef20aab113c78726eb038972edd8b72be3916e06
Sujay Naik <sujaynaik142@gmail.com>
2026-08-04 17:57:41 +0530

    feat: intercept response for 401 fetch new access token using refresh token and retry previous request

--- apps/shell/src/pages/Dashboard/DashboardPage.tsx
+++ apps/shell/src/pages/Dashboard/DashboardPage.tsx
@@ -1,6 +1,8 @@
 import { Button, Heading, Text } from "@commerce/ui";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../auth/useAuth";
+import { useEffect } from "react";
+import { authService } from "src/services/auth.service";
 
 export function DashboardPage() {
   const navigate = useNavigate();
@@ -10,6 +12,14 @@ export function DashboardPage() {
     logout();
     navigate("/login");
   }
+
+  useEffect(() => {
+    async function api() {
+      const response = await authService.me();
+      console.log(response);
+    }
+    api()
+  }, [])
 
   return (
     <>
--- apps/shell/src/services/api/interceptors.ts
+++ apps/shell/src/services/api/interceptors.ts
@@ -1,5 +1,6 @@
 import { apiClient } from "./client";
 import { tokenService } from "../token.service";
+import { authService } from "../auth.service";
 
 export function setupInterceptors() {
   apiClient.interceptors.request.use((config) => {
@@ -12,11 +13,33 @@ export function setupInterceptors() {
 
   apiClient.interceptors.response.use(
     response => response,
-    error => {
-        if (error.response?.status === 401) {
-            tokenService.clear();
-        }
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response?.status !== 401) {
         return Promise.reject(error);
+      }
+
+      if (originalRequest._retry) {
+        return Promise.reject(error);
+      }
+
+      originalRequest._retry = true;
+
+      const refreshToken = tokenService.getRefreshToken();
+      if (!refreshToken) {
+        tokenService.clear();
+        return Promise.reject(error);
+      }
+
+      const response = await authService.refresh(refreshToken);
+      tokenService.setAccessToken(
+          response.accessToken
+      );
+
+      originalRequest.headers.Authorization = `Bearer ${response.accessToken}`;
+
+      return apiClient(originalRequest);
     }
   );
 }
